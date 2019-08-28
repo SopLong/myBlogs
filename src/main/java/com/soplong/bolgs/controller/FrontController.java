cacdba12b158cab65322b35e729553685747ef3d
jc <915257899@qq.com>
2019-08-28 21:56:49 +0800

    博客 thymeleaf内容替换

--- src/main/java/com/soplong/bolgs/controller/FrontController.java
+++ src/main/java/com/soplong/bolgs/controller/FrontController.java
@@ -1,15 +1,18 @@
 package com.soplong.bolgs.controller;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.soplong.bolgs.pojo.article.ArticleContent;
 import com.soplong.bolgs.pojo.article.ArticleInfo;
+import com.soplong.bolgs.service.article.ArticleContentService;
 import com.soplong.bolgs.service.article.ArticleInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,20 +22,28 @@ import java.util.Map;
 public class FrontController {
     @Autowired
     private ArticleInfoService articleInfoService;
+    @Autowired
+    private ArticleContentService articleContentServicel;
 
     @GetMapping("/index")
-    public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView("/index");
-        Map<String,Object> map = new HashMap<>();
-        List<ArticleInfo> articleInfos = articleInfoService.selectList(new EntityWrapper<ArticleInfo>().eq("del_flag", 0));
-        map.put("data",articleInfos);
-        map.put("pageNum",Math.rint(articleInfos.size()/10));
-        modelAndView.addObject("aracleInfo",map);
-        return modelAndView;
+    public String index(Model model, @RequestParam(defaultValue = "1") Integer pageNum, @RequestParam(defaultValue = "1") Integer pageSize) {
+        Map<String,String> map = new HashMap<>();
+        Page page = new Page();
+        page.setSize(pageSize);
+        page.setCurrent(pageNum);
+        Page articlePage = articleInfoService.getArticleList(map, page);
+        model.addAttribute("articleInfos", articlePage.getRecords());
+        model.addAttribute("total", Math.rint(page.getTotal() / pageSize) == 0 ? 1 : Math.rint(page.getTotal() / pageSize));
+        model.addAttribute("currentPage", pageNum);
+        return "index";
     }
 
     @GetMapping("/singlePost")
-    public String singlePost() {
+    public String singlePost(Model model,@RequestParam Integer id) {
+        ArticleInfo articleInfo = articleInfoService.selectById(id);
+        ArticleContent articleContent = articleContentServicel.selectOne(new EntityWrapper<ArticleContent>().eq("article_id", id));
+        model.addAttribute("detail",articleInfo);
+        model.addAttribute("content",articleContent);
         return "single-post";
     }
 
